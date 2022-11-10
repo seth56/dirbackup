ecc5869c399c7e7f0a5b8cc64d5c1a7115a30622
seth56 <47400976+seth56@users.noreply.github.com>
2022-11-10 16:51:02 -0500

    Add files via upload

---
+++ backupproject.py
@@ -0,0 +1,48 @@
+import shutil
+from datetime import datetime
+import time
+import os
+
+colors = "\u001b["
+red = colors + "31m"
+reset = colors + "0m"
+green = colors + "32m"
+cyan = colors + "36m"
+blue = colors + "34m"
+yellow = colors + "33m"
+magenta = colors + "35m"
+black = colors + "30m"
+bg_cyan = colors + "46m"
+bg_magenta = colors + "45m"
+bold = colors + "1m"
+underline = colors + "4m"
+
+print("\n" + bg_cyan + magenta + "*****************************************")
+print(bold + black +             "          WELCOME TO BACKUP.PY!          ")
+print(magenta +                  "*****************************************" + reset + "\n")
+
+src = input(cyan + "Enter source to copy: " + reset)
+while src == "":
+    src = input(cyan + "Enter source to copy: " + reset)
+
+rootdir = '/home/seth/Documents/Backups/'
+print("\n" + magenta + "FOLDERS IN CURRENT DIRECTORY" + reset)
+for file in os.listdir(rootdir):
+        if "." not in file:
+            print("   ~ " + file)
+
+dest = input("\n" + cyan + "Enter destination: " + reset)
+while dest == "":
+    dest = input(cyan + "Enter destination: " + reset)
+minutes = input(cyan + "Enter the minutes in between each backup: " + reset)
+while minutes == "":
+    minutes = input(cyan + "Enter the minutes in between each backup: " + reset)
+
+print("\nRunning every " + str(minutes) + " minutes.")
+
+while True:
+    now = datetime.now()
+    dt_string = str(now.strftime("%d-%m-%Y_%H:%M:%S"))
+    shutil.copytree(src, dest + "/" + dt_string)
+    print(dest + "/" + dt_string + magenta + " CREATED" + reset)
+    time.sleep(int(minutes)*60)
